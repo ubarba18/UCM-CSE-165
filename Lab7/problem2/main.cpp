6f467d8b6b65ac83386e274b3d10d0b907483b1e
Uriel Barba <ubarba@ucmerced.edu>
2022-11-21 18:12:48 -0800

    init

---
+++ Lab7/problem2/main.cpp
@@ -0,0 +1,15 @@
+/**
+ Create a simple class (e.g., mySimpleClass) containing an int (e.g., int x) and overload 
+the addition operator (+) and the multiplication operator (*) as two member functions. Also provide a print( ) 
+member function that takes an ostream& (e.g., cout) as an argument (e.g., void print(ostream& out)) and print x to 
+that ostream& (e.g., out << x).
+*/
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class mySimpleClass
+ {
+    
+};
